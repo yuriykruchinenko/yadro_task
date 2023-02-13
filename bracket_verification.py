79f0aabdd14d0bc49b8fa4bc951ac8e98f91afc8
yuriykruchinenko <yuriykruchinenko@gmail.com>
2023-02-13 22:34:04 +0300

    init

---
+++ bracket_verification.py
@@ -0,0 +1,25 @@
+def check(string):
+    open_brack = ["[", "{", "("]
+    close_brack = ["]", "}", ")"]
+
+    stack = []  # stack for keeping income open_brackets
+    for i in string:
+        if i in open_brack:
+            stack.append(i)
+        elif i in close_brack:
+            pos = close_brack.index(i)
+            if ((len(stack) > 0) and
+                    (open_brack[pos] == stack[len(stack) - 1])):
+                stack.pop()
+            else:
+                return "False"
+    if len(stack) == 0:
+        return "True"
+    else:
+        return "False"
+
+
+string_verif = "{{}{}()}()[])"  # string to verify
+
+if __name__ == '__main__':
+    print(string_verif, 'is', check(string_verif))
